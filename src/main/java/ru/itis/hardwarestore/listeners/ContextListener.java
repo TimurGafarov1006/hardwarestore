9a0f544873bcc05011228fc6116ead6fafa1b95c
GafrikJr <gafrikjr22848@gmail.com>
2025-10-23 20:39:51 +0300

    feat: add context listener

---
+++ src/main/java/ru/itis/hardwarestore/listeners/ContextListener.java
@@ -0,0 +1,32 @@
+package ru.itis.hardwarestore.listeners;
+
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import ru.itis.hardwarestore.repositories.repositoryImpl.SessionRepositoryJdbc;
+import ru.itis.hardwarestore.repositories.repositoryImpl.UserRepositoryJdbc;
+import ru.itis.hardwarestore.repositories.repositoryInterfaces.SessionRepository;
+import ru.itis.hardwarestore.repositories.repositoryInterfaces.UserRepository;
+import ru.itis.hardwarestore.services.serviceImpl.SecurityServiceImpl;
+import ru.itis.hardwarestore.services.serviceInterfaces.SecurityService;
+import ru.itis.hardwarestore.util.PropertiesUtil;
+
+import java.time.Duration;
+import java.util.Properties;
+
+@WebListener
+public class ContextListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        Properties properties = PropertiesUtil.getProperties();
+        Duration sessionDuration = Duration.ofMinutes(Integer.parseInt(properties.getProperty("sessionDuration")));
+
+        UserRepository userRepository = new UserRepositoryJdbc();
+        SessionRepository sessionRepository = new SessionRepositoryJdbc();
+
+        SecurityService securityService = new SecurityServiceImpl(userRepository, sessionRepository);
+
+        sce.getServletContext().setAttribute("sessionDuration", sessionDuration);
+        sce.getServletContext().setAttribute("securityService", securityService);
+    }
+}
