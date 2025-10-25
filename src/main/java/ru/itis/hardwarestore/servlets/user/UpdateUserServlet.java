30c7da8df2b806f59752278df51211fa506900b8
GafrikJr <gafrikjr22848@gmail.com>
2025-10-25 16:49:40 +0300

    feat: add update controller + view

---
+++ src/main/java/ru/itis/hardwarestore/servlets/user/UpdateUserServlet.java
@@ -0,0 +1,78 @@
+package ru.itis.hardwarestore.servlets.user;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ru.itis.hardwarestore.exceptions.RegistrationValidateException;
+import ru.itis.hardwarestore.exceptions.UnauthorizedException;
+import ru.itis.hardwarestore.models.User;
+import ru.itis.hardwarestore.services.serviceInterfaces.UserService;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+@WebServlet("/cabinet/update")
+public class UpdateUserServlet extends HttpServlet {
+    private UserService userService;
+
+    @Override
+    public void init() throws ServletException {
+        this.userService = (UserService) getServletContext().getAttribute("userService");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = (String) req.getAttribute("userId");
+
+        try {
+            req.setAttribute("user", userService.getUser(userId));
+        } catch (UnauthorizedException e) {
+            resp.sendRedirect(req.getContextPath() + "/login");
+        }
+
+        req.getRequestDispatcher("/WEB-INF/views/user/update.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User oldUser = userService.getUser((String) req.getAttribute("userId"));
+
+        String firstName = req.getParameter("first_name");
+
+        String lastName = req.getParameter("last_name");
+
+        // если пользак ввел что-то в форму, то будем передавать этот параметр, иначе null
+        String password = (req.getParameter("password") != null && !req.getParameter("password").isBlank())
+                ? req.getParameter("password") : null;
+
+        // если пользак не ввел ничего и в форме остался старый параметр - передаем null, иначе новый параметр
+        String phone = (req.getParameter("phone").equals(oldUser.getPhone()))
+                ? null : req.getParameter("phone");
+
+        String email = (req.getParameter("email").equals(oldUser.getEmail()))
+                ? null : req.getParameter("email");
+
+        // если пользак не выбрал ДР - то null, иначе либо ДР при регистрации, либо только что введённый
+        LocalDate birthday = (req.getParameter("birthday") != null && !req.getParameter("birthday").isBlank())
+                ? LocalDate.parse(req.getParameter("birthday")) : null;
+
+        //проверка на то, обновил ли пользователь хоть один параметр
+        if (! (firstName.equals(oldUser.getFirstName()) &&
+                lastName.equals(oldUser.getLastName()) &&
+                password == null && phone == null &&
+                email == null && birthday != null))
+        {
+            try {
+                userService.updateUser(oldUser, firstName, lastName, password, phone, email, birthday);
+            } catch (RegistrationValidateException e) {
+                req.setAttribute("error", e.getMessage());
+                req.setAttribute("user", oldUser);
+                req.getRequestDispatcher("/WEB-INF/views/user/update.jsp").forward(req, resp);
+            }
+        }
+
+        resp.sendRedirect(req.getContextPath() + "/cabinet");
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/user/update.jsp
@@ -0,0 +1,52 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <c:if test="${not empty error}">
+      <div class="error-message">
+        <h4>Ошибка обновления данных</h4>
+        <p>${error}</p>
+      </div>
+    </c:if>
+    <form action="${pageContext.request.contextPath}/logout" method="post">
+      <button type="submit">Выйти из аккаунта</button>
+    </form>
+
+    <a href="${pageContext.request.contextPath}/cabinet">Личные данные</a>
+    <a href="${pageContext.request.contextPath}/cabinet/history">История заказов</a>
+    <a href="${pageContext.request.contextPath}/cabinet/discount-card">Скидочная карта</a>
+
+    <form method="POST" action="${pageContext.request.contextPath}/cabinet/update">
+      <label for="first_name">Имя:</label>
+      <textarea name="first_name" id="first_name">${user.firstName}</textarea>
+
+      <label for="last_name">Фамилия:</label>
+      <textarea name="last_name" id="last_name">${user.lastName}</textarea>
+
+      <label for="password">Новый пароль:</label>
+      <input type="password" name="password" id="password"/>
+
+      <label for="phone">Телефон:</label>
+      <textarea name="phone" id="phone">${user.phone}</textarea>
+
+      <label for="email">Почта:</label>
+      <textarea name="email" id="email">${user.email}</textarea>
+
+      <c:if test="${user.birthday!=null}">
+        <label for="birthday">День рождения:</label>
+        <input type="date" name="birthday" id="birthday" value="${user.birthday}" readonly>
+      </c:if>
+
+      <c:if test="${user.birthday==null}">
+        <label for="birthday">День рождения:</label>
+        <input type="date" name="birthday" id="birthday" >
+      </c:if>
+
+      <button type="submit">Сохранить изменения</button>
+
+    </form>
+</body>
+</html>
