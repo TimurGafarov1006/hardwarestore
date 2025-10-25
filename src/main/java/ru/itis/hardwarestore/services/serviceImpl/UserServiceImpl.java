42a1bfb3bc3d6f36d70c415309d0068b92da7326
GafrikJr <gafrikjr22848@gmail.com>
2025-10-25 16:48:43 +0300

    faet: add updateUser() method

--- src/main/java/ru/itis/hardwarestore/services/serviceImpl/UserServiceImpl.java
+++ src/main/java/ru/itis/hardwarestore/services/serviceImpl/UserServiceImpl.java
@@ -1,11 +1,17 @@
 package ru.itis.hardwarestore.services.serviceImpl;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import ru.itis.hardwarestore.exceptions.UnauthorizedException;
 import ru.itis.hardwarestore.models.User;
 import ru.itis.hardwarestore.repositories.repositoryInterfaces.UserRepository;
 import ru.itis.hardwarestore.services.serviceInterfaces.UserService;
+import ru.itis.hardwarestore.utils.PhoneUtils;
+import ru.itis.hardwarestore.utils.ValidationUtils;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.UUID;
 
 public class UserServiceImpl implements UserService {
     private UserRepository userRepository;
@@ -23,5 +29,38 @@ public class UserServiceImpl implements UserService {
         } else {
             throw new UnauthorizedException("Invalid session");
         }
+    }
+
+    @Override
+    public void updateUser(User oldUser, String newFirstName, String newLastName,
+                           String newPassword, String newPhone, String newEmail, LocalDate birthday)
+    {
+        String passwordHash = oldUser.getPasswordHash();
+        String salt = oldUser.getSalt();
+        String normalizedNewPhone =
+                newPhone == null ? null : PhoneUtils.normalizePhone(newPhone);
+
+        if (newPassword != null) {
+            salt = UUID.randomUUID().toString();
+            passwordHash = DigestUtils.sha256Hex(newPassword + salt);
+        }
+
+        ValidationUtils.validateUser(newFirstName, newLastName, newPassword, normalizedNewPhone, newEmail, true);
+
+        User updatedUser = new User(
+                oldUser.getId(),
+                oldUser.getRole(),
+                newFirstName != null ? newFirstName : oldUser.getFirstName(),
+                newLastName != null ? newLastName : oldUser.getLastName(),
+                passwordHash,
+                salt,
+                normalizedNewPhone != null ? normalizedNewPhone : oldUser.getPhone(),
+                newEmail != null ? newEmail : oldUser.getEmail(),
+                birthday != null ? birthday : oldUser.getBirthday(),
+                oldUser.getCreatedAt(),
+                LocalDateTime.now()
+        );
+
+        userRepository.update(updatedUser);
     }
 }
--- src/main/java/ru/itis/hardwarestore/services/serviceInterfaces/UserService.java
+++ src/main/java/ru/itis/hardwarestore/services/serviceInterfaces/UserService.java
@@ -2,6 +2,10 @@ package ru.itis.hardwarestore.services.serviceInterfaces;
 
 import ru.itis.hardwarestore.models.User;
 
+import java.time.LocalDate;
+
 public interface UserService {
     User getUser(String userId);
+    void updateUser(User oldUser, String firstName, String lastName,
+                    String password, String phone, String email, LocalDate birthday);
 }
