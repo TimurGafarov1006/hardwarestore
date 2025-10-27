6a4dcbd5985bde2d85a90ae567274cd7e2ef9825
GafrikJr <gafrikjr22848@gmail.com>
2025-10-27 17:22:40 +0300

    feat: add ProductNotFoundException

---
+++ src/main/java/ru/itis/hardwarestore/exceptions/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package ru.itis.hardwarestore.exceptions;
+
+public class ProductNotFoundException extends RuntimeException {
+    public ProductNotFoundException(String message) {
+        super(message);
+    }
+}
