df3c315dbf7b97c77e26a1558365cd876647f6f2
GafrikJr <gafrikjr22848@gmail.com>
2025-10-27 22:39:51 +0300

    feat: add DiscountCard controller + view

---
+++ src/main/java/ru/itis/hardwarestore/servlets/user/DiscountCardServlet.java
@@ -0,0 +1,45 @@
+package ru.itis.hardwarestore.servlets.user;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ru.itis.hardwarestore.exceptions.DiscountCardException;
+import ru.itis.hardwarestore.models.DiscountCard;
+import ru.itis.hardwarestore.services.serviceInterfaces.DiscountCardService;
+
+import java.io.IOException;
+
+@WebServlet("/cabinet/discount-card")
+public class DiscountCardServlet extends HttpServlet {
+    private DiscountCardService discountCardService;
+
+    @Override
+    public void init() throws ServletException {
+        this.discountCardService = (DiscountCardService) getServletContext().getAttribute("discountCardService");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = (String) req.getAttribute("userId");
+        DiscountCard discountCard;
+
+        try {
+            discountCard = discountCardService.getDiscountCard(userId);
+        } catch (DiscountCardException e) {
+            discountCard = null;
+        }
+
+        req.setAttribute("discountCard", discountCard);
+        req.getRequestDispatcher("/WEB-INF/views/user/discount-card.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String userId = (String) req.getAttribute("userId");
+        discountCardService.openDiscountCard(userId);
+
+        resp.sendRedirect(req.getContextPath() + "/cabinet/discount-card");
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/user/discount-card.jsp
@@ -0,0 +1,27 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <a href="${pageContext.request.contextPath}/cabinet/update">Изменить личные данные</a>
+    <a href="${pageContext.request.contextPath}/cabinet/history">История заказов</a>
+    <a href="${pageContext.request.contextPath}/cabinet">Личные данные</a>
+
+    <c:if test="${discountCard==null}">
+        <p>У вас нет скидочной карты. Давайте откроем!</p>
+
+        <form action="${pageContext.request.contextPath}/cabinet/discount-card" method="post">
+            <button type="submit">Открыть скидочную карту</button>
+        </form>
+    </c:if>
+    <c:if test="${discountCard!=null}">
+        <p>${discountCard.cardType.name}</p>
+        <p>${discountCard.cardNo}</p>
+        <p>${discountCard.cardType.discountPercent}%</p>
+    </c:if>
+
+    <a href="${pageContext.request.contextPath}/catalog">Каталог</a>
+</body>
+</html>
