177707300c854f68da7449af7f73e0da321ffbe5
GafrikJr <gafrikjr22848@gmail.com>
2025-10-27 16:19:42 +0300

    feat: add CRUD methods to CategoryRepository

---
+++ src/main/java/ru/itis/hardwarestore/repositories/queries/CategoryQueries.java
@@ -0,0 +1,45 @@
+package ru.itis.hardwarestore.repositories.queries;
+
+public class CategoryQueries {
+    public static final String SAVE_SQL = """
+            INSERT
+            INTO categories (name, parent_id, slug, created_at, updated_at)
+            VALUES (?, ?, ?, ?, ?)
+            """;
+    public static final String FIND_ALL_SQL = """
+            SELECT *
+            FROM categories
+            """;
+    public static final String FIND_BY_PARENT_ID_SQL = """
+            SELECT *
+            FROM categories
+            WHERE parent_id = ?
+            """;
+    public static final String FIND_BY_SLUG_SQL = """
+            SELECT *
+            FROM categories
+            WHERE slug = ?
+            """;
+    public static final String FIND_BY_PARENT_ID_IS_NULL_SQL = """
+            SELECT *
+            FROM categories
+            WHERE parent_id IS NULL
+            """;
+    public static final String UPDATE_SQL = """
+            UPDATE categories
+            SET name = ?,
+                parent_id = ?,
+                slug = ?,
+                updated_at = ?
+            WHERE id = ?
+            """;
+    public static final String DELETE_SQL = """
+            DELETE FROM categories
+            WHERE id = ?
+            """;
+    public static final String FIND_BY_ID_SQL = """
+            SELECT *
+            FROM categories
+            WHERE id = ?
+            """;
+}
---
+++ src/main/java/ru/itis/hardwarestore/repositories/repositoryImpl/CategoryRepositoryJdbc.java
@@ -0,0 +1,192 @@
+package ru.itis.hardwarestore.repositories.repositoryImpl;
+
+import ru.itis.hardwarestore.models.Category;
+import ru.itis.hardwarestore.repositories.repositoryInterfaces.CategoryRepository;
+import ru.itis.hardwarestore.utils.PropertiesUtil;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Properties;
+
+import static ru.itis.hardwarestore.repositories.queries.CategoryQueries.*;
+
+public class CategoryRepositoryJdbc implements CategoryRepository {
+    private final Properties properties;
+    private final String url;
+
+    public CategoryRepositoryJdbc() {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        properties = PropertiesUtil.getProperties();
+        url = properties.getProperty("url");
+    }
+
+    @Override
+    public void save(Category category) {
+        try (Connection connection = DriverManager.getConnection(url, properties);
+             PreparedStatement statement = connection.prepareStatement(SAVE_SQL))
+        {
+            statement.setString(1, category.getName());
+            statement.setInt(2, category.getId());
+            statement.setString(3, category.getSlug());
+            statement.setObject(4, category.getCreatedAt(), Types.TIMESTAMP);
+            statement.setObject(5, category.getUpdatedAt(), Types.TIMESTAMP);
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void update(Category category) {
+        try (Connection connection = DriverManager.getConnection(url, properties);
+             PreparedStatement statement = connection.prepareStatement(UPDATE_SQL))
+        {
+            statement.setString(1, category.getName());
+            statement.setInt(2, category.getId());
+            statement.setString(3, category.getSlug());
+            statement.setObject(4, category.getCreatedAt(), Types.TIMESTAMP);
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void delete(int id) {
+        try (Connection connection = DriverManager.getConnection(url, properties);
+             PreparedStatement statement = connection.prepareStatement(DELETE_SQL))
+        {
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<Category> findAll() {
+        List<Category> categories = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection(url, properties);
+             PreparedStatement statement = connection.prepareStatement(FIND_ALL_SQL);
+             ResultSet resultSet = statement.executeQuery())
+        {
+            while (resultSet.next()) {
+                Category category = new Category(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getInt("parent_id"),
+                        resultSet.getString("slug"),
+                        resultSet.getTimestamp("created_at").toLocalDateTime(),
+                        resultSet.getTimestamp("updated_at").toLocalDateTime()
+                );
+                categories.add(category);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return categories;
+    }
+
+    @Override
+    public List<Category> findByParentId(Integer parentId) {
+        List<Category> categories = new ArrayList<>();
+
+        try (Connection connection = DriverManager.getConnection(url, properties))
+        {
+            PreparedStatement statement;
+            if (parentId == null) {
+                statement = connection.prepareStatement(FIND_BY_PARENT_ID_IS_NULL_SQL);
+            } else {
+                statement = connection.prepareStatement(FIND_BY_PARENT_ID_SQL);
+                statement.setInt(1, parentId);
+            }
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                Category category = new Category(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        parentId != null ? resultSet.getInt("parent_id") : null,
+                        resultSet.getString("slug"),
+                        resultSet.getTimestamp("created_at").toLocalDateTime(),
+                        resultSet.getTimestamp("updated_at").toLocalDateTime()
+                );
+                categories.add(category);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return categories;
+    }
+
+    @Override
+    public Optional<Category> findById(int id) {
+        try (Connection connection = DriverManager.getConnection(url, properties);
+             PreparedStatement statement = connection.prepareStatement(FIND_BY_ID_SQL))
+        {
+            statement.setInt(1, id);
+
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                Category category = new Category(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getInt("parent_id"),
+                        resultSet.getString("slug"),
+                        resultSet.getTimestamp("created_at").toLocalDateTime(),
+                        resultSet.getTimestamp("updated_at").toLocalDateTime()
+                );
+
+                return Optional.ofNullable(category);
+            }
+
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Category> findBySlug(String slug) {
+        try (Connection connection = DriverManager.getConnection(url, properties);
+             PreparedStatement statement = connection.prepareStatement(FIND_BY_SLUG_SQL))
+        {
+            statement.setString(1, slug);
+
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                Category category = new Category(
+                        resultSet.getInt("id"),
+                        resultSet.getString("name"),
+                        resultSet.getInt("parent_id"),
+                        resultSet.getString("slug"),
+                        resultSet.getTimestamp("created_at").toLocalDateTime(),
+                        resultSet.getTimestamp("updated_at").toLocalDateTime()
+                );
+
+                return Optional.ofNullable(category);
+            }
+
+            resultSet.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return Optional.empty();
+    }
+}
