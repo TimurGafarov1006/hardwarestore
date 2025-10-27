37644769d7eea53249728ceeda7c1065f41d1035
GafrikJr <gafrikjr22848@gmail.com>
2025-10-27 22:14:15 +0300

    feat: add DiscountCardException

---
+++ src/main/java/ru/itis/hardwarestore/exceptions/DiscountCardException.java
@@ -0,0 +1,7 @@
+package ru.itis.hardwarestore.exceptions;
+
+public class DiscountCardException extends RuntimeException {
+    public DiscountCardException(String message) {
+        super(message);
+    }
+}
