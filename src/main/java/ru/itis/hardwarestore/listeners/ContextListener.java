8ce2ba377435753dc0e48f293d6280ce472f67bb
GafrikJr <gafrikjr22848@gmail.com>
2025-10-27 22:39:25 +0300

    feat: add new dependencies

--- src/main/java/ru/itis/hardwarestore/listeners/ContextListener.java
+++ src/main/java/ru/itis/hardwarestore/listeners/ContextListener.java
@@ -3,22 +3,10 @@ package ru.itis.hardwarestore.listeners;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
-import ru.itis.hardwarestore.repositories.repositoryImpl.CategoryRepositoryJdbc;
-import ru.itis.hardwarestore.repositories.repositoryImpl.ProductRepositoryJdbc;
-import ru.itis.hardwarestore.repositories.repositoryImpl.SessionRepositoryJdbc;
-import ru.itis.hardwarestore.repositories.repositoryImpl.UserRepositoryJdbc;
-import ru.itis.hardwarestore.repositories.repositoryInterfaces.CategoryRepository;
-import ru.itis.hardwarestore.repositories.repositoryInterfaces.ProductRepository;
-import ru.itis.hardwarestore.repositories.repositoryInterfaces.SessionRepository;
-import ru.itis.hardwarestore.repositories.repositoryInterfaces.UserRepository;
-import ru.itis.hardwarestore.services.serviceImpl.AuthServiceImpl;
-import ru.itis.hardwarestore.services.serviceImpl.CategoryServiceImpl;
-import ru.itis.hardwarestore.services.serviceImpl.ProductServiceImpl;
-import ru.itis.hardwarestore.services.serviceImpl.UserServiceImpl;
-import ru.itis.hardwarestore.services.serviceInterfaces.AuthService;
-import ru.itis.hardwarestore.services.serviceInterfaces.CategoryService;
-import ru.itis.hardwarestore.services.serviceInterfaces.ProductService;
-import ru.itis.hardwarestore.services.serviceInterfaces.UserService;
+import ru.itis.hardwarestore.repositories.repositoryImpl.*;
+import ru.itis.hardwarestore.repositories.repositoryInterfaces.*;
+import ru.itis.hardwarestore.services.serviceImpl.*;
+import ru.itis.hardwarestore.services.serviceInterfaces.*;
 import ru.itis.hardwarestore.utils.PropertiesUtil;
 
 import java.time.Duration;
@@ -35,11 +23,13 @@ public class ContextListener implements ServletContextListener {
         SessionRepository sessionRepository = new SessionRepositoryJdbc();
         CategoryRepository categoryRepository = new CategoryRepositoryJdbc();
         ProductRepository productRepository = new ProductRepositoryJdbc();
+        DiscountCardRepository discountCardRepository = new DiscountCardRepositoryJdbc();
 
         AuthService authService = new AuthServiceImpl(userRepository, sessionRepository);
         UserService userService = new UserServiceImpl(userRepository);
         CategoryService categoryService = new CategoryServiceImpl(categoryRepository);
         ProductService productService = new ProductServiceImpl(productRepository);
+        DiscountCardService discountCardService = new DiscountCardServiceImpl(discountCardRepository);
 
         sce.getServletContext().setAttribute("sessionRepository", sessionRepository);
         sce.getServletContext().setAttribute("sessionDuration", sessionDuration);
@@ -47,5 +37,6 @@ public class ContextListener implements ServletContextListener {
         sce.getServletContext().setAttribute("authService", authService);
         sce.getServletContext().setAttribute("categoryService", categoryService);
         sce.getServletContext().setAttribute("productService", productService);
+        sce.getServletContext().setAttribute("discountCardService", discountCardService);
     }
 }
