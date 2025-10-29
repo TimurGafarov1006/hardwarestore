f4f5dfabe287de44a5dfca5ac4efe0634cabcc42
GafrikJr <gafrikjr22848@gmail.com>
2025-10-29 12:10:23 +0300

    feat: add new attribute for session

--- src/main/java/ru/itis/hardwarestore/servlets/catalog/ProductServlet.java
+++ src/main/java/ru/itis/hardwarestore/servlets/catalog/ProductServlet.java
@@ -9,6 +9,7 @@ import ru.itis.hardwarestore.models.Category;
 import ru.itis.hardwarestore.models.Product;
 import ru.itis.hardwarestore.services.serviceInterfaces.CategoryService;
 import ru.itis.hardwarestore.services.serviceInterfaces.ProductService;
+import ru.itis.hardwarestore.services.serviceInterfaces.UserService;
 
 import java.io.IOException;
 
@@ -16,11 +17,13 @@ import java.io.IOException;
 public class ProductServlet extends HttpServlet {
     private ProductService productService;
     private CategoryService categoryService;
+    private UserService userService;
 
     @Override
     public void init() throws ServletException {
         this.productService = (ProductService) getServletContext().getAttribute("productService");
         this.categoryService = (CategoryService) getServletContext().getAttribute("categoryService");
+        this.userService = (UserService) getServletContext().getAttribute("userService");
     }
 
     @Override
@@ -34,7 +37,9 @@ public class ProductServlet extends HttpServlet {
         String productSlug = pathInfo.substring(1).split("/")[0];
         Product product = productService.getProductBySlug(productSlug);
         Category productCategory = categoryService.getCategory(product.getCategoryId());
+        String userId = (String) req.getAttribute("userId");
 
+        req.setAttribute("user", userService.getUser(userId));
         req.setAttribute("product", product);
         req.setAttribute("productCategory", productCategory);
         req.getRequestDispatcher("/WEB-INF/views/catalog/specific-product.jsp").forward(req, resp);
