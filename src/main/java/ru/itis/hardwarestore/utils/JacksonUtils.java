8c0184cbf25ca2777e57afc37067084b0b2e0787
GafrikJr <gafrikjr22848@gmail.com>
2025-10-30 16:04:22 +0300

    feat: add method to convert JavaList to json

--- src/main/java/ru/itis/hardwarestore/utils/JacksonUtils.java
+++ src/main/java/ru/itis/hardwarestore/utils/JacksonUtils.java
@@ -3,13 +3,20 @@ package ru.itis.hardwarestore.utils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import ru.itis.hardwarestore.models.CartElement;
+import ru.itis.hardwarestore.models.Product;
+
+import java.util.List;
 
 public class JacksonUtils {
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     static {
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 
     public static String cartElementToJson(CartElement cartElement) {
@@ -27,4 +34,13 @@ public class JacksonUtils {
             throw new RuntimeException("Ошибка десериализации JSON в CartElement. JSON: " + json, e);
         }
     }
+
+    public static String ProductsListToJson(List<Product> products) {
+        try {
+            return objectMapper.writeValueAsString(products);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Ошибка сериализации списка ProductsList в JSON", e);
+        }
+    }
+
 }
