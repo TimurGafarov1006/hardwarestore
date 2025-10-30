ab862197f4784a60b52fb6c6b7695e7b93bd006f
GafrikJr <gafrikjr22848@gmail.com>
2025-10-30 22:56:31 +0300

    feat: add new mapping for OrderServlet

--- src/main/java/ru/itis/hardwarestore/servlets/user/OrderServlet.java
+++ src/main/java/ru/itis/hardwarestore/servlets/user/OrderServlet.java
@@ -5,11 +5,14 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import ru.itis.hardwarestore.models.Order;
+import ru.itis.hardwarestore.models.Product;
 import ru.itis.hardwarestore.services.serviceInterfaces.OrderService;
 
 import java.io.IOException;
+import java.util.Map;
 
-@WebServlet("/cabinet/orders")
+@WebServlet("/cabinet/orders/*")
 public class OrderServlet extends HttpServlet {
     private OrderService orderService;
 
@@ -20,9 +23,25 @@ public class OrderServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String pathInfo = req.getPathInfo();
         String userId = (String) req.getAttribute("userId");
-        req.setAttribute("orders", orderService.getOrders(userId));
-        req.getRequestDispatcher("/WEB-INF/views/user/orders.jsp").forward(req, resp);
+
+        if (pathInfo == null || pathInfo.equals("/") || pathInfo.isEmpty()) {
+            req.setAttribute("orders", orderService.getOrders(userId));
+            req.getRequestDispatcher("/WEB-INF/views/user/orders.jsp").forward(req, resp);
+        } else {
+            Integer orderId = Integer.parseInt(pathInfo.substring(1));
+
+            Map<Order, Map<Product, Integer>> orderData = orderService.getOrder(orderId);
+            Map.Entry<Order, Map<Product, Integer>> entry = orderData.entrySet().iterator().next();
+
+            Order order = entry.getKey();
+            Map<Product, Integer> products = entry.getValue();
+
+            req.setAttribute("products", products);
+            req.setAttribute("order", order);
+            req.getRequestDispatcher("/WEB-INF/views/user/order-details.jsp").forward(req, resp);
+        }
     }
 
     @Override
