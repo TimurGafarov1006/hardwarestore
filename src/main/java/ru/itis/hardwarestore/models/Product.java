a475fb0ccc6371a4ebd7fd57ce573247d2cb4759
GafrikJr <gafrikjr22848@gmail.com>
2025-11-01 09:15:45 +0300

    feat: add setter annotation for Product

--- src/main/java/ru/itis/hardwarestore/models/Product.java
+++ src/main/java/ru/itis/hardwarestore/models/Product.java
@@ -2,11 +2,13 @@ package ru.itis.hardwarestore.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @ToString
 public class Product {
